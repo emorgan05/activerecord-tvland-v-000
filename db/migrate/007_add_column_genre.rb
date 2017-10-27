77553e10cd90f729da50153c8a0479b91670ebbc
Emily Morgan <emorgan05@gmail.com>
2017-10-27 19:32:26 +0000

    automatically backed up by learn

--- db/migrate/007_add_column_genre.rb
+++ db/migrate/007_add_column_genre.rb
@@ -1,5 +1,7 @@
-class AddColumnGenre < ActiveRecord::Migration
+class AddColumnsToShows < ActiveRecord::Migration
   def change
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
     add_column :shows, :genre, :string
   end
 end

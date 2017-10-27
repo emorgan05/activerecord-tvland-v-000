41596e9118af2339bdf89330b7b6ecf0745f5dbc
Emily Morgan <emorgan05@gmail.com>
2017-10-27 19:42:24 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,6 @@ class Actor < ActiveRecord::Base
     roles = []
     self.characters.each do |character|
       roles << "#{character.name} - #{character.show.name}"
-      binding.pry
     end
     roles
   end
